58eb975b7561b9ac8d7263b80d214d9df4468cf5
Olga <pon29103@gmail.com>
2026-03-24 23:08:09 +0300

    update docker flow

---
+++ scripts/airflow-init.sh
@@ -0,0 +1,8 @@
+airflow db migrate &&
+  airflow users create
+  --username ${AIRFLOW_ADMIN_USERNAME}
+  --password ${AIRFLOW_ADMIN_PASSWORD}
+  --firstname ${AIRFLOW_ADMIN_FIRSTNAME}
+  --lastname ${AIRFLOW_ADMIN_LASTNAME}
+  --role Admin
+  --email ${AIRFLOW_ADMIN_EMAIL}
